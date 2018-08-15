d8da64fefacc1578f5e802a516a837bddf730aa8
杨俊宁 <1003719811@qq.com>
2018-08-15 09:55:39 +0800

    Bais -> BaisCell

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import React, {PureComponent} from 'react'
 import {View, Text} from 'react-native'
 import PropTypes from 'prop-types'
 
-export default class Bias extends PureComponent {
+export default class BiasCell extends PureComponent {
   static propTypes = {
     height: PropTypes.number,
     width: PropTypes.number,
